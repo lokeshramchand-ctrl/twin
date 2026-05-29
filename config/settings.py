2a45660168e0d847633233b23c00f653976a0372
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-05-29 11:55:56 +0530

    chore: remove obsolete files and scripts from the repository

--- config/settings.py
+++
@@ -1,2 +0,0 @@
-OLLAMA_URL="https://ollama.splsystems.in/api/chat"
-MODEL = "gemma4:latest"
--- schemas/chat_response.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class ChatResponse(
-    BaseModel
-):
-
-    answer:str
